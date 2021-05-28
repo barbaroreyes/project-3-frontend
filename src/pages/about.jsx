697415fc2b88ae11ec5bc27eb8fc5f519fefc9bf
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-28 13:56:49 -0500

    update

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="about">
+            <div>mika</div>
+            <div>daniel</div>
+            <div>ben</div>
+            <div>barbaro</div>
+            
+        </div>
+    )
+}
+ export default About
